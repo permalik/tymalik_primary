af0c3d3d25a3ae66ae4116ab301f63fae65c7784
permalik <permalik@protonmail.com>
2021-11-29 19:01:39 -0600

    remove allArticles page

--- src/pages/allArticles.js
+++ src/pages/allArticles.js
@@ -1,46 +1,46 @@
-import fs from 'fs';
-import { sortByDate } from '../../utils';
-import matter from 'gray-matter';
-import path from 'path';
+// import fs from 'fs';
+// import { sortByDate } from '../../utils';
+// import matter from 'gray-matter';
+// import path from 'path';
 
-import ArticleCard from '../components/ArticleCard';
-import PrimarySection from '../components/PrimarySection';
+// import ArticleCard from '../components/ArticleCard';
+// import PrimarySection from '../components/PrimarySection';
 
-import ArticlesStyles from '../styles/Articles.module.scss';
+// import ArticlesStyles from '../styles/Articles.module.scss';
 
-export default function AllArticles({ articles }) {
-  return (
-    <PrimarySection>
-      all articles
-      {articles.map((article, index) => (
-        <ArticleCard key={index} article={article} />
-      ))}
-    </PrimarySection>
-  );
-}
+// export default function AllArticles({ articles }) {
+//   return (
+//     <PrimarySection>
+//       all articles
+//       {articles.map((article, index) => (
+//         <ArticleCard key={index} article={article} />
+//       ))}
+//     </PrimarySection>
+//   );
+// }
 
-export async function getStaticProps() {
-  const files = fs.readdirSync(path.join('articles'));
+// export async function getStaticProps() {
+//   const files = fs.readdirSync(path.join('articles'));
 
-  const articles = files.map((filename) => {
-    const slug = filename.replace('.md', '');
+//   const articles = files.map((filename) => {
+//     const slug = filename.replace('.md', '');
 
-    const markdownWithMeta = fs.readFileSync(
-      path.join('articles', filename),
-      'utf-8'
-    );
+//     const markdownWithMeta = fs.readFileSync(
+//       path.join('articles', filename),
+//       'utf-8'
+//     );
 
-    const { data: frontmatter } = matter(markdownWithMeta);
+//     const { data: frontmatter } = matter(markdownWithMeta);
 
-    return {
-      slug,
-      frontmatter
-    };
-  });
+//     return {
+//       slug,
+//       frontmatter
+//     };
+//   });
 
-  return {
-    props: {
-      articles: articles.sort(sortByDate)
-    }
-  };
-}
+//   return {
+//     props: {
+//       articles: articles.sort(sortByDate)
+//     }
+//   };
+// }
