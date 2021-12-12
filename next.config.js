2fe8f781e3c80a859d9aa42ef1ad54bee4e38578
permalik <permalik@protonmail.com>
2021-12-11 19:45:02 -0600

    Add next-i18next functionality and config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const { i18n } = require('./next-i18next.config');
+
 module.exports = {
   images: {
     domains: ['res.cloudinary.com']
   },
-  reactStrictMode: true
+  reactStrictMode: true,
+  i18n
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import App from 'next/app';
-import Head from 'next/head';
+import { appWithTranslation } from 'next-i18next';
 
 import Layout from '../components/Layout';
+
 import '../styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }) => {
@@ -13,10 +12,4 @@ const MyApp = ({ Component, pageProps }) => {
   );
 };
 
-// MyApp.getInitialProps = async (appContext) => {
-//   const appProps = await App.getInitialProps(appContext);
-
-//   return { ...appProps };
-// };
-
-export default MyApp;
+export default appWithTranslation(MyApp);
