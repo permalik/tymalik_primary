4b3218af6008e571e7de8fa75fe107c6ac39e87c
permalik <permalik@protonmail.com>
2021-12-11 23:17:13 -0600

    Update accessibility for allTrials page component

--- src/components/PuzzleCard/PuzzleCard.js
+++ src/components/PuzzleCard/PuzzleCard.js
@@ -4,16 +4,22 @@ import PuzzleCardStyles from '../../styles/PuzzleCard.module.scss';
 
 export default function PuzzleCard({ trial }) {
   return (
-    <Link href={`${trial.link}`} passHref>
-      <article className={PuzzleCardStyles.article}>
-        <header className={PuzzleCardStyles.header}>
-          <h2 className={PuzzleCardStyles.title}>{trial.title}</h2>
-          <p className={PuzzleCardStyles.description}>
-            <span>{trial.language}</span> | <span>{trial.type}</span>
-          </p>
-        </header>
-        <time className={PuzzleCardStyles.timestamp}>{trial.timestamp}</time>
-      </article>
-    </Link>
+    <li className={PuzzleCardStyles.listItem}>
+      <Link href={`${trial.link}`} passHref>
+        <a>
+          <div className={PuzzleCardStyles.article}>
+            <header className={PuzzleCardStyles.header}>
+              <h2 className={PuzzleCardStyles.title}>{trial.title}</h2>
+              <p className={PuzzleCardStyles.description}>
+                <span>{trial.language}</span> | <span>{trial.type}</span>
+              </p>
+            </header>
+            <time className={PuzzleCardStyles.timestamp}>
+              {trial.timestamp}
+            </time>
+          </div>
+        </a>
+      </Link>
+    </li>
   );
 }
--- src/pages/allTrials.js
+++ src/pages/allTrials.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import PrimarySection from '../components/PrimarySection';
 import PuzzleCard from '../components/PuzzleCard';
 import puzzleCardArray from '../../utils/puzzleCardArray';
@@ -7,6 +9,15 @@ import AllTrialStyles from '../styles/AllTrials.module.scss';
 export default function AllArticles() {
   return (
     <PrimarySection>
+      <Head>
+        <title>Ty Malik | Trial Archive</title>
+        <meta name='description' content="Ty Malik's trial archive" />
+        <meta
+          name='keywords'
+          content='software development, html, css, javascript, codewars'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <h1 className={AllTrialStyles.headingOne}>all trials</h1>
       <ul className={AllTrialStyles.puzzleList}>
         {puzzleCardArray.map((trial, index) => (
