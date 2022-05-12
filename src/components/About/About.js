16ebf56f2d0c95a91d4416e4291f533fad554132
permalik <permalik@protonmail.com>
2022-05-12 18:38:44 -0500

    Update About component summary

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,18 +14,16 @@ export default function About() {
         />
         <p className={AboutStyles.aboutDescription}>
           <span>
-            I&#39;m Ty Malik, a designer and front-end engineer. I&#39;ve been
-            developing websites and applications for the past two years.
+            I&#39;m a designer and front-end engineer.
           </span>
           <span>
-            While maintaining a full-time job, I am both working toward a
-            computer science degree and following the LaunchSchool programming
-            curriculum.
+            My focus is on building functional, effective, and appealing experiences.
           </span>
           <span>
-            The life-long learning process is what initially led me to software
-            engineering and it&#39;s that which keeps me interested. I&#39;m
-            currently based in Oklahoma City, OK.
+            I&#39;m currently learning to implement animations in web applications and sites.
+          </span>
+          <span>
+            cheers.
           </span>
         </p>
       </div>
