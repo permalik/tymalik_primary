c6142f2ed5db4acdeff0a8b2acf2f12c8eb19349
permalik <permalik@protonmail.com>
2022-05-18 16:14:40 -0500

    Update styles for themed skillsetCards

--- src/theme/ThemeConfig.js
+++ src/theme/ThemeConfig.js
@@ -1,18 +1,27 @@
-import { createGlobalStyle } from 'styled-components';
+import {createGlobalStyle} from 'styled-components';
 
 export const lightTheme = {
     body: '#FFF',
+    skillsetCardBackground: '#FFF',
+    skillsetCardColor: '#03010D',
     text: '#03010D'
-}
+};
 
 export const darkTheme = {
     body: '#03010D',
+    skillsetCardBackground: '#1C182F',
+    skillsetCardColor: '#FFF',
     text: '#FFF'
-}
+};
 
 export const GlobalStyles = createGlobalStyle`
-body {
-  background: ${({ theme }) => theme.body};
-  color: ${({ theme }) => theme.text};
-}
-`
+  body, a {
+    background: ${({theme}) => theme.body};
+    color: ${({theme}) => theme.text};
+  }
+  
+  li {
+    background: ${({theme}) => theme.skillsetCardBackground};
+    color: ${({theme}) => theme.skillsetCardColor};
+  }
+`;
