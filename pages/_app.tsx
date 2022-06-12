c22d360cc7f291d4a9ed41ea6ccbedb1cd9486df
Malik <permalik@protonmail.com>
2022-06-12 14:04:45 -0500

    Update dotenv config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: __dirname+'/.env' });
+require('dotenv').config({ path: '.env' });
 
 import {appWithTranslation} from "next-i18next";
 import {AppProps} from "next/app";
