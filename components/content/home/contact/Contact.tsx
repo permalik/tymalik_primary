58dae815500638f58068b808049331840d01d979
Malik <permalik@protonmail.com>
2022-06-15 20:08:29 -0500

    Implement new Contact form validation and functionality

--- components/content/home/contact/Contact.tsx
+++ components/content/home/contact/Contact.tsx
@@ -1,43 +1,57 @@
-import {useFormik} from "formik";
-import {useState} from "react";
-import * as yup from "yup";
+import emailjs from "@emailjs/browser";
+import {useRef} from "react";
+import {useForm} from "react-hook-form";
 
 import Section from "@components/content/home/common/section";
 
 import styles from "./Contact.module.scss";
 
+interface IFormInputs {
+  name: string;
+  email: string;
+  message: string;
+};
+
+// const schema = Yup.object({
+//   name: Yup
+//     .string()
+//     .required("Name is required"),
+//   email: Yup
+//     .string()
+//     .email()
+//     .required("Email is required"),
+//   message: Yup
+//     .string()
+//     .required("Message is required")
+// });
+
 const Contact = () => {
+  const form = useRef<HTMLFormElement>(null);
+  const textArea = useRef<HTMLTextAreaElement>(null);
 
-  const [message, setMessage] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  const {
+    formState: {errors},
+    handleSubmit,
+    register,
+    reset
+  } = useForm<IFormInputs>();
 
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      message: ""
-    },
-    onSubmit: () => {
-      setMessage("Form submitted");
-      setSubmitted(true);
-    },
-    validationSchema: yup.object({
-      name: yup.string().trim().required("Name is required"),
-      email: yup
-        .string()
-        .email("Must be a valid email")
-        .required("Email is required"),
-      message: yup.string().trim().required("Message is required")
-    })
-  });
+  const sendEmail = (e: any) => {
+    emailjs.sendForm("service_tymalik", "template_tymalik", form.current!, "DqrRy_3JD7E59rcF9")
+      .then((result) => {
+        alert(result.text);
+      }, (error) => {
+        alert(error.text);
+      });
+  };
 
   return (
     <div id={"contact"}>
       <Section heading={"say hi :)"}>
-        <p hidden={!submitted}>{message}</p>
         <form
           className={styles.form}
-          onSubmit={formik.handleSubmit}
+          onSubmit={handleSubmit(sendEmail)}
+          ref={form}
         >
           <label
             className={styles.contactLabel}
@@ -46,19 +60,27 @@ const Contact = () => {
             Name
           </label>
           <input
+            {...register("name", {
+              pattern: /^[A-Za-z]+$/i,
+              required: true
+            })}
             className={styles.contactInput}
             id="name"
             name="name"
-            onChange={formik.handleChange}
-            required
             type="text"
-            value={formik.values.name}
           />
-          {formik.errors.name && (
-            <p className={styles.errorMessage}>
-              {formik.errors.name}
-            </p>
-          )}
+          {errors?.name?.type === "required"
+            &&
+              <p className={styles.errorMessage}>
+                  Name is required.
+              </p>
+          }
+          {errors?.name?.type === "pattern"
+            &&
+              <p className={styles.errorMessage}>
+                  Alphabetical characters only.
+              </p>
+          }
           <label
             className={styles.contactLabel}
             htmlFor="email"
@@ -66,18 +88,26 @@ const Contact = () => {
             Email
           </label>
           <input
+            {...register("email", {
+              pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              required: true
+            })}
             className={styles.contactInput}
             name="email"
-            onChange={formik.handleChange}
-            required
             type="email"
-            value={formik.values.email}
           />
-          {formik.errors.email && (
-            <p className={styles.errorMessage}>
-              {formik.errors.email}
-            </p>
-          )}
+          {errors?.email?.type === "required"
+            &&
+              <p className={styles.errorMessage}>
+                  Email is required.
+              </p>
+          }
+          {errors?.name?.type === "pattern"
+            &&
+              <p className={styles.errorMessage}>
+                  Input a valid email.
+              </p>
+          }
           <label
             className={styles.contactLabel}
             htmlFor="message"
@@ -87,15 +117,8 @@ const Contact = () => {
           <textarea
             className={styles.contactTextArea}
             name="message"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            required
-            value={formik.values.message}/>
-          {formik.errors.message && (
-            <p className={styles.errorMessage}>
-              {formik.errors.message}
-            </p>
-          )}
+            ref={textArea}
+          />
           <button
             className={styles.contactSubmit}
             type="submit"
