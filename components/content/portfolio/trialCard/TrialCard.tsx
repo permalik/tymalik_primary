36e7818d851fd5cac1a59b31daba6ab330b168ff
Malik <permalik@protonmail.com>
2022-06-21 16:38:05 -0500

    Update trialCard to styled-component

--- components/content/portfolio/trialCard/TrialCard.tsx
+++ components/content/portfolio/trialCard/TrialCard.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
+import styled from "styled-components";
 
 import styles from "./TrialCard.module.scss";
+
+const StyledLink = styled.a`
+  background: #1C182F;
+  color: #FFF;
+`;
 
 const TrialCard = ({trial}: any) => {
   return (
     <li className={styles.card}>
       <Link href={`${trial.link}`} passHref>
-        <a>
+        <StyledLink>
           <header className={styles.header}>
             <h3>
               {trial.title}
@@ -18,7 +24,7 @@ const TrialCard = ({trial}: any) => {
           <p className={styles.description}>
             A {trial.type} challenge solved with {trial.language}
           </p>
-        </a>
+        </StyledLink>
       </Link>
     </li>
   );
